e5b75b5e23ffff99a25c0ea3a4df635f5a71f6c1
Tomislav Miljak <tomislav.miljak@dci-student.org>
2024-01-18 10:17:10 +0100

    Tomislav step1 commit

---
+++ step1.js
@@ -0,0 +1,17 @@
+class Worker {
+    constructor(name, position) {
+      this.name = name;
+      this.position = position;
+      
+    }
+  }
+  
+  const worker1 = new Worker("Worlanyo", "barista");
+  const worker2 = new Worker("Ognjen", "barista");
+  const worker3 = new Worker("Lucas", "cashier");
+  const worker4 = new Worker("Tomislav", "cashier");
+
+  console.log(worker1);
+  console.log(worker2);
+  console.log(worker3);
+  console.log(worker4);
